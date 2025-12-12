519c96bfe4d998bc68b718ed3be657d7e6f3175b
sanchess2803 <88556220+mateuscostasanches@users.noreply.github.com>
2025-12-12 18:38:44 -0300

    Add files via upload

---
+++ 2374 - Pneu.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int pressaoDesejada;
+    int pressaoLida;
+
+    cin >> pressaoDesejada;
+    cin >> pressaoLida;
+    
+    cout << pressaoDesejada - pressaoLida << endl;
+    return 0;
+}
